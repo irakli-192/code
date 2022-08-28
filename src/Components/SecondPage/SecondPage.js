861e6e0ffbd25f1008e5958c72b5f32540fe8833
ika-192 <ichkh2017@agruni.edu.ge>
2022-08-28 15:05:37 +0400

    form html structure

--- src/Components/SecondPage/SecondPage.js
+++ src/Components/SecondPage/SecondPage.js
@@ -9,11 +9,44 @@ function SecondPage() {
       <img src={Group4} className='returnmark'/>
       <div className='main_box'>
         <div className='headers'>
-          <h2>თანამშრომლის ინფო</h2>
+          <div>
+            <h2 className='personal_info'>თანამშრომლის ინფო</h2>
+            <div className='black_line'></div>
+          </div>
           <h2>ლეპტოპის მახასიათებლები</h2>
         </div>
         <div className='box'>
-
+          <form>
+            <div className='first_part'>
+              <div>
+                <label>სახელი</label>
+                <input type='text' placeholder='გრიშა'/>
+                <span>მინიმუმ 2 სიმბოლო, ქართული ასოები</span>
+              </div>
+              <div>
+                <label>გვარი</label>
+                <input type='text' placeholder='ბაგრატიონი'/>
+                <span>მინიმუმ 2 სიმბოლო, ქართული ასოები</span>
+              </div>
+            </div>
+            <select>
+              <option>თიმი</option>
+            </select>
+            <select>
+              <option>პოზიცია</option>
+            </select>
+            <div>
+              <label>მეილი</label>
+              <input type='text' placeholder='grisha666@redberry.ge'/>
+              <span>უნდა მთავრდებოდეს redberry.ge-ით</span>
+            </div>
+            <div>
+              <label>ტელეფონის ნომერი</label>
+              <input type='text' placeholder='+995 598 00 07 01'/>
+              <span>უნდა აკმაყოფილებდეს ქართული მობ-ნომრის ფორმატს</span>
+            </div>
+            <button>შემდეგი</button>
+          </form>
         </div>
         <img src={logo2} alt='Redberry Logo' className='RedLogo'/>
       </div>
