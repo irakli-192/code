3f1b24b36d2fd1a6f2098f5824787bca83085c8b
ika-192 <ichkh2017@agruni.edu.ge>
2022-08-28 14:34:26 +0400

    Employee page marku-up part one

--- src/Components/SecondPage/SecondPage.js
+++ src/Components/SecondPage/SecondPage.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './SecondPage.css'
+import logo2 from '../../assets/logo2.png'
+import Group4 from '../../assets/Group4.png'
 
 function SecondPage() {
   return (
     <div>
-      SecondPage
+      <img src={Group4} className='returnmark'/>
+      <div className='main_box'>
+        <div className='headers'>
+          <h2>თანამშრომლის ინფო</h2>
+          <h2>ლეპტოპის მახასიათებლები</h2>
+        </div>
+        <div className='box'>
+
+        </div>
+        <img src={logo2} alt='Redberry Logo' className='RedLogo'/>
+      </div>
     </div>
   )
 }
