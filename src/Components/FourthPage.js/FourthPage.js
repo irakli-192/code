208d11f6956ece3f43c8ab733fac36b989fae7f0
ika-192 <ichkh2017@agruni.edu.ge>
2022-09-01 20:47:22 +0400

    return main page from last page

--- src/Components/FourthPage.js/FourthPage.js
+++ src/Components/FourthPage.js/FourthPage.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './FourthPage.css'
 import Frame from '../../assets/Frame.png'
+import { navContext } from '../Context/navContext'
 function FourthPage() {
+  const{setPage}=useContext(navContext)
+  const mainPage=(e)=>{
+    e.preventDefault();
+    setPage(2)
+  }
   return (
     <div className='gray_box'>
       <div className='content'>
@@ -11,7 +17,7 @@ function FourthPage() {
         <h1 className='addRecord'>ჩანაწერი დამატებულია!</h1>
         <div className='FourthPageButtons'>
           <button className='list_button'>სიაში გადაყვანა</button>
-          <button className='welcomeButton'>მთავარი</button>
+          <button className='welcomeButton' onClick={mainPage}>მთავარი</button>
         </div>
       </div>
     </div>
